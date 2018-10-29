30d3511ee9a99531f837455e2e960f608f2a834a
EVAOTA <nervevawaka@ezweb.ne.jp>
2018-10-30 01:10:31 +0900

    fixed commets and messages

--- python/SmartLocker.py
+++ python/SmartLocker.py
@@ -1,11 +1,10 @@
 from BLEcontroller import BLEcontroller
 from bluepy.btle import *
 from time import sleep
-import struct
 
 class SmartLoker():
     def __init__(self,openParam=1000,closeParam=2000):
-        self.close_comm        = 0
+        self.close_comm         = 0
         self.open_comm          = 1
         self.close_param_comm   = 2
         self.open_param_comm    = 3
@@ -23,11 +22,26 @@ class SmartLoker():
 
         self.ble.connect()
 
+        # switch servo definitation
+        # 0: close
+        # 1: open
+        # 2: set open parameter
+        # 3: set close parameter
+        
+        # setting sequense
+        # 1. write open parameters
+        # 2. write No.2 to switch servo characteristic to store open parameter in device
+        # 3. compare whether the transmitted parameter is the same as the value received from status characteristic
+        # 4. write close parameters
+        # 5. write No.3 to switch servo characteristic to store close parameter in device
+        # 6. compare whether the transmitted parameter is the same as the value received from status characteristic
+        # 7. write No.0 to switch servo characteristic
+
         self.ble.write("short","servo_param",self.openParameter)
         self.ble.write("byte","switch_servo",self.open_param_comm)
         response = self.ble.read("short","status")
         if(response == self.openParameter):
-            print("success set open paramter[%d] return [%d]" % (self.openParameter,response))
+            print("Success to set open paramter. wrote paramter:[%d] response from device:[%d]" % (self.openParameter,response))
         else:
             print("Faild set parameter %d" % response)
 
@@ -35,7 +49,7 @@ class SmartLoker():
         self.ble.write("byte","switch_servo",self.close_param_comm)
         response = self.ble.read("short","status")
         if(response == self.closeParameter):
-            print("success set close paramter[%d] return [%d]" % (self.closeParameter,response))
+            print("Success to set close paramter. wrote paramter:[%d] response from device:[%d]" % (self.closeParameter,response))
         else:
             print("Faild set parameter %d" % response)
 
@@ -43,47 +57,26 @@ class SmartLoker():
 
 
     def getServoStatus(self):
-        tmp = self.ble.read("status")
-        print("%d"%tmp)
-        if tmp is self.openParameter:
+        response = self.ble.read("short","status")
+        if(response == self.openParameter):
             return True
         else: 
             return False
     
     def operateServo(self,boolean=False):
         if(boolean is True):
-            self.ble.write("servo",self.openParameter)
+            self.ble.write("byte","switch_servo",self.open_comm)
         else:
-            self.ble.write("servo",self.closeParameter)
-
+            self.ble.write("byte","switch_servo",self.close_comm)
+        
 
 if __name__ == '__main__':
     locker = SmartLoker( )
     i = 0
     try:
-        while True:
-            pass
-            # locker.ble.write("short","servo",1200)
-            # print(locker.ble.read("short","status"))
-            # locker.operateServo(True)
-            # sleep(3)
-            # print(locker.getServoStatus())
-            # locker.operateServo(False)
-            # sleep(3)          
-            # print(locker.getServoStatus())
+        pass
+        # while True:
+        #     pass
     except KeyboardInterrupt:
         exit()
-    # switch servo
-    # 0: close
-    # 1: open
-    # 2: set open parameter
-    # 3: set close parameter
     
-    # setting sequense
-    # 1. write open parameters
-    # 2. write No.2 switch servo characteristic to store open parameter in device
-    # 3. compare whether the transmitted parameter is the same as the value received from status characteristic
-    # 4. write close parameters
-    # 5. write No.3 switch servo characteristic to store close parameter in device
-    # 6. compare whether the transmitted parameter is the same as the value received from status characteristic
-    # 7. write No.0 switch servo characteristic
